1e97c0d107371327fc9a168953d28eff8545a324
Akul85 <biswasakul287@gmail.com>
2025-08-08 23:17:08 +0600

    added banner of menue and used paraleelx

--- bistro-boss-client/src/Pages/Menu/Menu/Menu.jsx
+++ bistro-boss-client/src/Pages/Menu/Menu/Menu.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import imgBanner from '../../../assets/banner3.jpg'
+import PopularMenu from '../../Home/PopularMenu/PopularMenu'
 function Menu(props) {
     return (
         <div>
             <Helmet>
                 <title>Bistro | Menu</title>
             </Helmet>
+            <Cover img={imgBanner} title="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={imgBanner} title="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={imgBanner} title="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
         </div>
     );
 }
